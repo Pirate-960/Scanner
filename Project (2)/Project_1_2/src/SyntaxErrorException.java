d4713ad41569d0fb1d0e9002f70590d93a38ef8f
Abdelrahman Zahran <101503244+Pirate-960@users.noreply.github.com>
2023-05-26 08:58:24 +0300

    Add files via upload

---
+++ Project (2)/Project_1_2/src/SyntaxErrorException.java
@@ -0,0 +1,13 @@
+//----------------------------------------------------------------------------------------------------//
+// Contributers:-
+// - Muhammed Enes Gökdeniz (150121538)
+// - Tolga Fehmioğlu (150120022)
+// - Abdelrahman Zahran (150120998)
+//----------------------------------------------------------------------------------------------------//
+/** SyntaxErrorException **/
+public class SyntaxErrorException extends RuntimeException {
+    public SyntaxErrorException(String message){
+        super(message);
+    }
+}
+//----------------------------------------------------------------------------------------------------//
